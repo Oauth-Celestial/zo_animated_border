da968075477044b230c4103454cb52e9b2768428
Ravi Pai <ravi.983365@gmail.com>
2026-04-26 15:16:35 +0530

    Fixed path issue

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
-      home: const ExZoFireBorder(),
+      home: const ExZoSegmentBorder(),
     );
   }
 }
